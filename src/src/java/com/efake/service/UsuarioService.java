3a1e243409a6a86bba4c404fd0161ff58a2d9709
PedroArs99 <PJAP@uma.es>
2020-04-08 03:37:11 +0200

    2:00 Pedro, Created service for hashing passwords, designed landing page

---
+++ src/src/java/com/efake/service/UsuarioService.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.efake.service;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author PedroArenas
+ */
+@Stateless
+public class UsuarioService {
+    
+    public byte[] hashPassword(String password){
+        byte[] hash = null;
+        
+        try {
+            //Create Hash algorithm instance for SHA-256
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            
+            //Set text that's gonna be hashed in UTF-8 encoding
+            md.update(password.getBytes("UTF-8"));
+            //Apply hash function
+            hash = md.digest();
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            Logger.getLogger(UsuarioService.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            return hash;
+        }
+    }
+    
+}
