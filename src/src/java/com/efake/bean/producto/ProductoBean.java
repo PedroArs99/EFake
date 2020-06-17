a87d3008773121cb5bb2b5d76c6c6e1e4e489899
JuanMedinaMoya <0619706393@uma.es>
2020-06-17 20:55:34 +0200

    crear y visualizacion del producto

--- src/src/java/com/efake/bean/producto/ProductoBean.java
+++ src/src/java/com/efake/bean/producto/ProductoBean.java
@@ -1,9 +1,14 @@
 package com.efake.bean.producto;
 
 import com.efake.bean.session.Transport;
+import com.efake.dao.CategoriaFacade;
+import com.efake.dao.KeywordsFacade;
 import com.efake.dao.ProductoFacade;
 import com.efake.dto.ProductoDTO;
 import com.efake.dto.ValoracionDTO;
+import com.efake.entity.Categoria;
+import com.efake.entity.Keywords;
+import com.efake.entity.Usuario;
 import com.efake.entity.Valoracion;
 import com.efake.service.ProductoService;
 import java.util.ArrayList;
@@ -31,11 +36,22 @@ public class ProductoBean {
     @EJB
     private ProductoFacade productoFacade;
     
+    @EJB
+    private CategoriaFacade categoriafacade;
+    
     private ProductoDTO producto;
     private double mediaValoraciones;
     private List<Valoracion> listaValoraciones;
     private List<Integer> ratings;
     private boolean valorado;
+    private String imagen;
+    private String nombre;
+    private String descripcion;
+    private List<Keywords> listakeywords;
+    private Double precio;
+    private Integer id;
+    private Usuario owner;
+ 
     
     public ProductoBean() {
     }
@@ -52,6 +68,14 @@ public class ProductoBean {
         for(Integer i : ratingsDictionary.values()){
             ratings.add(i);
         }
+        imagen = producto.getImagen();
+        descripcion = producto.getDescripcion();
+        nombre = producto.getNombre();
+        listakeywords = producto.getListaKeywords();
+        precio = producto.getPrecio();
+        id = producto.getId();
+        owner = producto.getOwner();
+        
         //valorado = this.productoService.rated(listaValoraciones, user);
     }
 
@@ -102,5 +126,64 @@ public class ProductoBean {
         }
         return sb.toString();
     }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<Keywords> getListakeywords() {
+        return listakeywords;
+    }
+
+    public void setListakeywords(List<Keywords> listakeywords) {
+        this.listakeywords = listakeywords;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Usuario getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Usuario owner) {
+        this.owner = owner;
+    }
+    
+    
+    
     
 }
