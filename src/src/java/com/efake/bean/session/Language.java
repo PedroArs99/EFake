851146d037e6bc215893869d32c7f0f18eb40049
PedroArs99 <PJAP@uma.es>
2020-06-22 01:12:50 +0200

    Added support for internacionalization

---
+++ src/src/java/com/efake/bean/session/Language.java
@@ -0,0 +1,56 @@
+package com.efake.bean.session;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+
+/**
+ *
+ * @author PedroArenas
+ */
+@Named(value = "language")
+@SessionScoped
+public class Language implements Serializable {
+
+    private String localeCode;
+
+    private static Map<String, Object> countries;
+
+    static {
+        countries = new LinkedHashMap<>();
+        countries.put("English", Locale.ENGLISH); //label, value
+        countries.put("Spanish", new Locale("es"));
+    }
+
+    public Map<String, Object> getCountriesInMap() {
+        return countries;
+    }
+
+    public String getLocaleCode() {
+        return localeCode;
+    }
+
+    public void setLocaleCode(String localeCode) {
+        this.localeCode = localeCode;
+    }
+
+    //value change event listener
+    public void changeLocale(String newLocale) {
+        //loop country map to compare the newLocale code
+        for (Map.Entry<String, Object> entry : countries.entrySet()) {
+
+            if (entry.getValue().toString().equals(newLocale)) {
+
+                FacesContext.getCurrentInstance()
+                        .getViewRoot().setLocale((Locale) entry.getValue());
+
+            }
+        }
+    }
+
+}
--- src/src/java/com/efake/bean/session/Transport.java
+++ src/src/java/com/efake/bean/session/Transport.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.efake.bean.session;
 
-import com.efake.dao.ProductoFacade;
 import com.efake.dto.CategoriaDTO;
 import com.efake.dto.ProductoDTO;
 import com.efake.dto.SubCategoriaDTO;
 import com.efake.dto.UsuarioDTO;
-import com.efake.entity.Producto;
 import com.efake.service.CategoryService;
 import com.efake.service.ProductoService;
 import com.efake.service.SubCategoryService;
@@ -36,7 +29,7 @@ public class Transport implements Serializable {
     private SubCategoryService subCategoriaService;
     @EJB
     private ProductoService productoService;
-    
+
     //Attributes
     private List<CategoriaDTO> listaCategoria;
     private List<SubCategoriaDTO> listaSubCategoria;
@@ -45,18 +38,18 @@ public class Transport implements Serializable {
     private CategoriaDTO categoriaSeleccionada;
     private String search;
     private UsuarioDTO toEditUser;
-    
+
     //Constructor
     public Transport() {
     }
-    
+
     @PostConstruct
-    public void init(){
+    public void init() {
         this.listaCategoria = this.categoriaService.findAll();
         this.listaSubCategoria = this.subCategoriaService.findAll();
     }
-    
-    public List<SubCategoriaDTO> getSubCategoriasByCategoria(CategoriaDTO c){
+
+    public List<SubCategoriaDTO> getSubCategoriasByCategoria(CategoriaDTO c) {
         return this.subCategoriaService.finByCategory(c);
     }
 
@@ -99,15 +92,15 @@ public class Transport implements Serializable {
     public void setCategoriaSeleccionada(CategoriaDTO categoriaSeleccionada) {
         this.categoriaSeleccionada = categoriaSeleccionada;
     }
-    
+
     public String getSearch() {
         return search;
     }
-    
-    public void setSearch(String search){
+
+    public void setSearch(String search) {
         this.search = search;
     }
-    
+
     public UsuarioDTO getToEditUser() {
         return toEditUser;
     }
@@ -118,41 +111,38 @@ public class Transport implements Serializable {
 
     public String redirectCategory(CategoriaDTO categoria) {
         this.categoriaSeleccionada = categoria;
-        
+
         return "productGrid?faces-redirect=true";
     }
-        
-    public String redirectProducto(Integer idProducto){
+
+    public String redirectProducto(Integer idProducto) {
         productoSeleccionado = this.productoService.findById(idProducto);
-        
+
         return "producto?faces-redirect=true";
     }
-    
-    public String redirectEditUser(UsuarioDTO user){
+
+    public String redirectEditUser(UsuarioDTO user) {
         toEditUser = user;
-        
+
         return "editUser?faces-redirect=true";
     }
-    
-    public String sellProduct(){
+
+    public String sellProduct() {
         productoSeleccionado = null;
-        
+
         return "productForm?faces-redirect=true";
-    }
-    public String modifyProcut(Integer idProducto){
-        productoSeleccionado = this.productoService.findById(idProducto);
-        
-        return "productForm?faces-redirect=true";
-    }
-    
-    public String deleteProduct(Integer idProducto){
-        productoSeleccionado = this.productoService.findById(idProducto);
-        productoService.delete(productoSeleccionado);
-        
-        return "index?faces-redirect=true";
     }
 
-    
-    
-    
+    public String modifyProcut(Integer idProducto) {
+        productoSeleccionado = this.productoService.findById(idProducto);
+
+        return "productForm?faces-redirect=true";
+    }
+
+    public String deleteProduct(Integer idProducto) {
+        productoSeleccionado = this.productoService.findById(idProducto);
+        productoService.delete(productoSeleccionado);
+
+        return "index?faces-redirect=true";
+    }
 }
